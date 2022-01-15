d41fec8a44c058607aa6fef8fc7c0a52df595b8d
unknown <valerijamax@gmail.com>
2022-01-15 18:04:49 +0200

    program that creates a phone book storing names and phone numbers of your contacts from a fileat creates a phone book storing names and phone numbers of your contacts from a file.

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func createPhoneBook(names []string, numbers []string) map[string]string {
+	m := make(map[string]string)
+	for i := range names {
+		m[names[i]] = numbers[i]
+	}
+	return m
+}
+func main() {
+	f, err := os.Open("phone_numbers.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	var names, numbers []string
+	i := 0
+	for s.Scan() {
+		if i == 0 {
+			names = append(names, s.Text())
+			i = 1
+		} else {
+			numbers = append(numbers, s.Text())
+			i = 0
+		}
+	}
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(createPhoneBook(names, numbers))
+}
