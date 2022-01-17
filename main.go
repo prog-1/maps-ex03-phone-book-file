f26917309399a574d26511c6fad93bbbc3e6e635
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2022-01-17 22:05:37 +0200

    added some changes to main.go

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 
 func createPhoneBook(names []string, numbers []string) map[string]string {
 	m := make(map[string]string)
-	for i := len(names) - 1; i >= 0; i-- {
-		m[names[i]] = numbers[i]
+	for i, key := range names { // or as I did before:         for i := len(names) - 1; i >= 0; i-- {
+		if _, found := m[key]; !found { //                             m[names[i]] = numbers[i]
+			m[key] = numbers[i] //                             }
+		} //                                                   return m
 	}
 	return m
 }
@@ -24,16 +26,25 @@ func main() {
 
 	var names, numbers []string
 	s := bufio.NewScanner(f)
-	isname := true
 	for s.Scan() {
-		if isname {
-			names = append(names, s.Text())
-			isname = false
-		} else {
-			numbers = append(numbers, s.Text())
-			isname = true
-		}
+		names = append(names, s.Text())
+		s.Scan()
+		numbers = append(numbers, s.Text())
 	}
+
+	// or as I did before:
+	//
+	// isname := true
+	// for s.Scan() {
+	//	   if isname {
+	//		   names = append(names, s.Text())
+	//		   isname = false
+	//	   } else {
+	//		   numbers = append(numbers, s.Text())
+	//		   isname = true
+	//	   }
+	// }
+
 	if err := s.Err(); err != nil {
 		log.Fatalf("scanning: %v", err) // or just log.Fatal(err)
 	}
